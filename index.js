53a180394b6c87f674c03e8366cd60574e09bfed
Isaac Novaes <isaac.novaes@hotmail.com>
2021-08-27 00:21:35 +0200

    Update files. Final form is achieved

---
+++ index.js
@@ -0,0 +1,91 @@
+const form = document.querySelector("form"),
+	formContainer = document.querySelector("div.form-container");
+let correctInputs = true;
+
+form.addEventListener("submit", validateForm);
+
+function validateForm(event) {
+	event.preventDefault();
+	const firstName = document.getElementById("firstName"),
+		lastName = document.getElementById("lastName"),
+		email = document.getElementById("email"),
+		password = document.getElementById("password");
+
+	validateName(firstName);
+	validateName(lastName);
+	validateEmail(email);
+	validatePassword(password);
+
+	if (correctInputs) {
+		showResultSubmission(formContainer);
+	}
+}
+
+function showError(element) {
+	element.nextElementSibling.className = "show-error-icon";
+	element.nextElementSibling.nextElementSibling.style.visibility = "visible";
+	element.style.border = "2px solid rgba(255, 0, 0, 0.883)";
+	correctInputs = false;
+}
+function showCorrect(element) {
+	element.nextElementSibling.className = "show-error-icon";
+	element.nextElementSibling.style.backgroundImage = "url(images/checked-icon.svg)";
+	element.nextElementSibling.nextElementSibling.style.visibility = "hidden";
+	element.style.border = "2px solid hsl(154, 59%, 51%)";
+	correctInputs = true;
+}
+
+function showSubmission(element) {
+	element.innerHTML = "Your data has been submitted";
+	element.style.color = "hsl(154, 59%, 51%)";
+	element.style.textAlign = "center";
+	element.style.height = "60px";
+}
+function validateName(name) {
+	if (name.value.trim() == "") {
+		showError(name);
+	} else {
+		showCorrect(name);
+	}
+}
+
+function validateEmail(name) {
+	if (
+		!/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+			name.value.trim()
+		)
+	) {
+		showError(name);
+	} else {
+		showCorrect(name);
+	}
+}
+
+function validatePassword(name) {
+	if (name.value.trim() == "") {
+		showError(name);
+	} else if (name.value.trim().length <= 6) {
+		showError(name);
+		name.nextElementSibling.nextElementSibling.textContent = "Password should have more than 6 characters.";
+	} else {
+		showCorrect(name);
+	}
+}
+
+function showResultSubmission(element) {
+	const inputs = document.querySelectorAll("div.input");
+	let counter = 0;
+
+	inputs.forEach((item) => {
+		item.style.animationDelay = `${counter++ / 1.5}s`;
+	});
+	setInterval(() => {
+		inputs.forEach((item) => {
+			item.classList.add("animate");
+		});
+	}, 1000);
+
+	setInterval(() => {
+		showSubmission(element);
+	}, 4000);
+}
